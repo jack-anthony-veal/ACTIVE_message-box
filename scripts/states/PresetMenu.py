db13ac6a40e956d015ab67c1078d2cc5a2f3f8d9
jack_a_veal <jackvealwork@gmail.com>
2026-07-10 14:05:18 +0100

    Fixed the navigation of presets and its title screen

--- scripts/states/PresetMenu.py
+++ scripts/states/PresetMenu.py
@@ -1,57 +1,74 @@
+import time
+
 class PresetMenu:
     def __init__(self, app):
         self.app = app
-        self.current_index = 1
-        self.index_switched = False
+        self.current_index_preset = 0
+        self.index_switched_preset = False
         self.preset_data_full = None
         self.preset_data_select = None
+        self.running=False
 
     def exit_state(self):
         ...
 
     def draw(self):
-        if self.index_switched:
-            self.preset_data_full = None
-            self.preset_data_select = self.preset_data_full[self.current_index]
+        if self.index_switched_preset:
+            self.preset_data_select = self.preset_data_full[self.current_index_preset]
+            time.sleep_ms(10)
             self.preset_body()
-            self.app.display.custom_message(self.preset_header(), fill_all=True, wrap=False)
-            self.app.display.custom_message(self.preset_data_select, x_axis=0, y_axis=8, fill_all=False, wrap=True)
-            self.index_switched = False
+            time.sleep_ms(10)
+            self.app.display.custom_message(self.preset_header(),x_axis=0, y_axis=0, fill_all=True, wrap=False)
+            self.app.display.custom_message(self.preset_data_select, x_axis=0, y_axis=8, wrap=True, fill_all=False)
+            self.index_switched_preset = False
+        else: return
 
 
     def handle_input(self, event, event_type):
         if event_type == 'button' and event is not None:
-            ...
+            print('button')
+            return
 
         if event_type == 'switch' and event is not None:
-            self.current_index += 1
-            self.index_switched = True
+            index_direction = 1 if event else -1  # Added to remove redundancy
+            self.current_index_preset = (self.current_index_preset + index_direction) % len(self.preset_data_full)
+            self.index_switched_preset = True
+            return
+        else:
             return
 
 
     def enter_state(self):
-        self.app.display.custom_message("Loading...", x_axis=0, y_axis=8, fill_all=True)
-
+        self.app.state_manager.current_state = self # ensure current is self
+        self.app.display.custom_message("Loading...", x_axis=0, y_axis=8, fill_all=True, wrap = False)
         new_data, preset_data = self.app.message_api.load_presets()
         self.preset_data_full = preset_data
-        self.preset_data_select = self.preset_data_full[self.current_index]
+        self.preset_data_select = str(self.preset_data_full[0])
 
-        self.index_switched = True
+        self.index_switched_preset = True
 
         return None
 
 
+    def update(self):
+        return
+
+
 
     def preset_header(self):
-        if type(self.preset_data_select) != list: raise Exception("DATA NOT IN LIST!!!")
+        index_current = self.current_index_preset  # calls current index
+        header_data = self.preset_data_full[index_current]
 
-        index_current = self.current_index  # calls current index
+        [header_data.join(str(line) + '\n') for line in header_data if type(header_data == list)]
 
-        str_ext = f'PRESET{index_current + 1}/'  # make changeable
-        str_sff = '...'
-        header = self.preset_data_select[index_current][:16 - (len(str_ext) + len(str_sff))]
+        str_ext: str = f'PRESET{str(index_current+1)}/'  # make changeable
+        str_sff: str = '...'
+        length_data: int = (16 - (len(str_ext) + len(str_sff)))
+        max_to_show: int = length_data if int(length_data) < 16 else 0
 
-        data_final = str_ext + header + str_sff
+        header_data = header_data[0:int(max_to_show)] if max_to_show > 0 else ''
+
+        data_final = str(str_ext) + str(header_data) + str(str_sff)
 
         return data_final  # returns heade
 
